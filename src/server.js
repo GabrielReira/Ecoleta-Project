8395106d014b2d0b23184ec4757d73c7952bd685
Gabriel Reira <62738460+GabrielReira@users.noreply.github.com>
2020-06-05 15:17:39 -0300

    modificando arquivos e adicionando Nunjucks

--- src/server.js
+++ src/server.js
@@ -4,19 +4,29 @@ const server = express()
 // Configurar pasta pública
 server.use(express.static("public"))
 
+
+// Template engine - Nunjucks
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
 // CONFIGURAÇÃO DOS CAMINHOS DA APLICAÇÃO
 // 1 - Página index
 server.get("/", (req, res) => { // Requisição e resposta
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html", { title: "Um título"})
 })
 // 2 - Página create-point
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
 })
 // 3 - Página search-results
 server.get("/search-results", (req, res) => { // Requisição e resposta
-    res.sendFile(__dirname + "/views/search-results.html")
+    return res.render("search-results.html")
 })
+
 
 // Ligar o servidor
 server.listen(3000)
